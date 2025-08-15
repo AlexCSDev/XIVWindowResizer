f42bff72cf198d6b0e7c373a281c71697f2e3f66
Craig Venz <craig.venz@gmail.com>
2025-08-15 15:20:21 -0700

    API update to 13
    - Moved some types into their own files.
    - Applied some Resharper cleanups.
    - Updated to Dalamud SDK 13.

--- XIVWindowResizer/Helpers/WindowSearchHelper.cs
+++ XIVWindowResizer/Helpers/WindowSearchHelper.cs
@@ -1,20 +1,15 @@
-using Dalamud.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace XIVWindowResizer.Helpers
 {
     public class WindowSearchHelper
     {
         [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string lclassName, string windowTitle);
+        public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string lClassName, string windowTitle);
 
         [DllImport("user32.dll")]
-        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int ProcessId);
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int processId);
 
         [DllImport("user32.dll")]
         public static extern bool IsWindowVisible(IntPtr hWnd);
@@ -24,7 +19,7 @@ namespace XIVWindowResizer.Helpers
             IntPtr handle = IntPtr.Zero;
             while ((handle = FindWindowEx(IntPtr.Zero, handle, "FFXIVGAME", "FINAL FANTASY XIV")) != IntPtr.Zero)
             {
-                _ = GetWindowThreadProcessId(handle, out int pid);
+                _ = GetWindowThreadProcessId(handle, out var pid);
 
                 if (pid == Environment.ProcessId && IsWindowVisible(handle))
                     break;
--- XIVWindowResizer/Plugin.cs
+++ XIVWindowResizer/Plugin.cs
@@ -1,36 +1,39 @@
 using Dalamud.Game.Command;
+using Dalamud.Game.Text;
 using Dalamud.IoC;
 using Dalamud.Plugin;
-using XIVWindowResizer.Helpers;
-using System.Drawing;
-using Dalamud.Game.Text;
-using Dalamud.Game.Gui;
-using System;
 using Dalamud.Plugin.Services;
+using JetBrains.Annotations;
+using System;
+using System.Drawing;
+using XIVWindowResizer.Helpers;
 
 namespace XIVWindowResizer
 {
     public sealed class Plugin : IDalamudPlugin
     {
+        [UsedImplicitly]
         public string Name => "XIVWindowResizer";
         private const string CommandName = "/wresize";
 
+        [UsedImplicitly]
         [PluginService]
-        private ICommandManager _commandManager { get; init; }
+        private ICommandManager CommandManager { get; init; }
 
+        [UsedImplicitly]
         [PluginService]
-        private IChatGui _chatGui { get; init; }
+        private IChatGui ChatGui { get; init; }
 
-        private Size _originalWindowSize { get; set; }
+        private Size OriginalWindowSize { get; set; }
 
-        private WindowSizeHelper _windowSizeHelper { get; init; }
+        private WindowSizeHelper WindowSizeHelper { get; init; }
 
         public Plugin()
         {
-            _windowSizeHelper = new WindowSizeHelper(new WindowSearchHelper());
-            _originalWindowSize = _windowSizeHelper.GetWindowSize();
+            WindowSizeHelper = new WindowSizeHelper(new WindowSearchHelper());
+            OriginalWindowSize = WindowSizeHelper.GetWindowSize();
 
-            _commandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+            CommandManager!.AddHandler(CommandName, new CommandInfo(OnCommand)
             {
                 HelpMessage = "Set window size.\r\nUsage:\r\n/wresize set <width> <height> - Set window size.\r\n/wresize reset - Reset window size back to the original size.\r\n/wresize update - Update window size used by /wresize reset command. Use if you have changed game's screen resolution without restarting the game or reloading the plugin."
             });
@@ -38,25 +41,25 @@ namespace XIVWindowResizer
 
         public void Dispose()
         {
-            _commandManager.RemoveHandler(CommandName);
+            CommandManager.RemoveHandler(CommandName);
         }
 
         private void OnCommand(string command, string args)
         {
-            string[] splitArgs = args.ToLowerInvariant().Split(' ');
+            var splitArgs = args.ToLowerInvariant().Split(' ');
             switch(splitArgs[0])
             {
                 case "set":
-                    int width = 0;
-                    int height = 0;
+                    var width = 0;
+                    var height = 0;
 
-                    bool isInvalidSize = false;
+                    var isInvalidSize = false;
                     try
                     {
                         width = int.Parse(splitArgs[1]);
                         height= int.Parse(splitArgs[2]);
                     }
-                    catch(Exception ex)
+                    catch (Exception)
                     {
                         isInvalidSize = true;
                     }
@@ -70,16 +73,16 @@ namespace XIVWindowResizer
                         return;
                     }
 
-                    _windowSizeHelper.SetWindowSize(width, height);
+                    WindowSizeHelper.SetWindowSize(width, height);
                     PrintInChat($"Window size is set to {width}x{height}");
                     break;
                 case "reset":
-                    _windowSizeHelper.SetWindowSize(_originalWindowSize.Width, _originalWindowSize.Height);
-                    PrintInChat($"Window size is reset to {_originalWindowSize.Width}x{_originalWindowSize.Height}");
+                    WindowSizeHelper.SetWindowSize(OriginalWindowSize.Width, OriginalWindowSize.Height);
+                    PrintInChat($"Window size is reset to {OriginalWindowSize.Width}x{OriginalWindowSize.Height}");
                     break;
                 case "update":
-                    _originalWindowSize = _windowSizeHelper.GetWindowSize();
-                    PrintInChat($"Updated saved window size to {_originalWindowSize.Width}x{_originalWindowSize.Height}");
+                    OriginalWindowSize = WindowSizeHelper.GetWindowSize();
+                    PrintInChat($"Updated saved window size to {OriginalWindowSize.Width}x{OriginalWindowSize.Height}");
                     break;
 				default:
                     PrintInChat($"Unknown command: {splitArgs[0]}");
@@ -94,7 +97,7 @@ namespace XIVWindowResizer
                 Message = message
             };
 
-            _chatGui.Print(xivChat);
+            ChatGui.Print(xivChat);
         }
     }
 }
